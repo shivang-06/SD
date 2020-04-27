919c4aa74b737ad2c31d33eeaf83770f694286b8
Shivang <shivang.sharma15@outlook.com>
2020-04-27 17:42:17 +0530

    updated file

--- Promises/Facts/promise1.js
+++ Promises/Facts/promise1.js
@@ -11,4 +11,4 @@ console.log(pendingPromise); // At this stage promise will always be pending
 
 setTimeout(function(){
     console.log(pendingPromise); // at this stage we will acquire the value of promise
-},2000) //we are printing after 2sec when resolve is called only after 1 sec this gap will let us acquire value of promise
+},1000) //we are printing after 2sec when resolve is called only after 1 sec this gap will let us acquire value of promise
