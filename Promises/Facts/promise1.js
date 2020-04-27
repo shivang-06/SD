177d7cf6599f935d6333aa2d46471e96f9bc800e
Shivang <shivang.sharma15@outlook.com>
2020-04-27 17:31:11 +0530

    added new file

---
+++ Promises/Facts/promise1.js
@@ -0,0 +1,14 @@
+function promiseCreator(){
+    return new Promise(function(resolve,reject){
+        setTimeout(function(){
+            resolve(10);
+        },1000)
+    })
+}
+
+let pendingPromise = promiseCreator()
+console.log(pendingPromise); // At this stage promise will always be pending
+
+setTimeout(function(){
+    console.log(pendingPromise); // at this stage we will acquire the value of promise
+},2000) //we are printing after 2sec when resolve is called only after 1 sec this gap will let us acquire value of promise
