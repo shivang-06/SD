f61d54f4029d774274f1bab1562c4bae7b0c2212
Shivang <shivang.sharma15@outlook.com>
2020-04-28 16:23:03 +0530

    new file added

--- concepts/read_File_conditional_Async.js
+++ concepts/read_File_conditional_Async.js
@@ -1,2 +1,17 @@
 let fs = require("fs");
 
+(async function(){
+    let f1 = await fs.promises.readFile("f1.txt","utf8");
+    console.log("LENTGH  "+ f1.length);
+    
+    if(f1.length>120){
+        let f2 = await fs.promises.readFile("f2.txt","utf8")
+        console.log("FILE IN IF : "+f2);
+        
+    }else{
+        let f3 = await fs.promises.readFile("f3.txt","utf8");
+        console.log("FILE IN ELSE+ "+f3);
+    }
+    console.log(f1);
+    
+})()
