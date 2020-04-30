a5ed35916197c4fe677b9d465e507d660120722f
Shivang <shivang.sharma15@outlook.com>
2020-04-30 22:48:38 +0530

    new file added

---
+++ HackerRank_Automation/Activity/creatChallenges.js
@@ -0,0 +1,12 @@
+let fs = require("fs")
+require("chromedriver")
+let swd = require("selenium-webdriver")
+let bldr = new swd.Builder();
+let driver = bldr.forBrowser("chrome").build()
+
+
+
+
+async function waitForLoader(){
+
+}
