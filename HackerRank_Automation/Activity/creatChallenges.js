73064a174f476af3247f1b52c492c8bdf8cfbea8
Shivang <shivang.sharma15@outlook.com>
2020-05-01 18:44:44 +0530

    updated file

--- HackerRank_Automation/Activity/creatChallenges.js
+++ HackerRank_Automation/Activity/creatChallenges.js
@@ -3,10 +3,26 @@ require("chromedriver")
 let swd = require("selenium-webdriver")
 let bldr = new swd.Builder();
 let driver = bldr.forBrowser("chrome").build()
+let cFile = process.argv[2];
 
 
+(async function(){
+    try{
+        await loginHelper();
+
+    }catch(err){
+        console.log(err);
+    }
+})()
 
 
 async function waitForLoader(){
+    let WillwaitForLoader = 
 
+}
+
+async function loginHelper(){
+    await driver.manage().setTimeout({implicit: 10000, pageLoad: 10000})
+    let data = await fs.promises.readFile(cFile);
+    let {user,pwd,}
 }
