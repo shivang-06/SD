1ec306e7154327c4ec8011b73d9e4dd39a28636f
Shivang <shivang.sharma15@outlook.com>
2020-05-03 20:59:53 +0530

    updated files

--- HackerRank_Automation/Activity/moderator.js
+++ HackerRank_Automation/Activity/moderator.js
@@ -12,14 +12,30 @@ let userToadd = process.argv[3];
     try {
         await loginHelper();
 
-        let 
+        let dropDownBtn = await driver.findElement(swd.By.css("a[data-analytics=NavBarProfileDropDown]"))
+        await dropDownBtn.click();
+        let adminLinAnchor = await driver.findElement(swd.By.css("a[data-analytics=NavBarProfileDropDownAdministration]"));
+        await adminLinAnchor.click();
+        let manageTabs = await driver.findElements(swd.By.css(".administration header ul li a"))
+        // console.log(manageTabs[1]);
+        
+        let href = await manageTabs[1].getAttribute("href")
+        // console.log(href);
+        let manageChallengeURL =  href;
+        await driver.get(manageChallengeURL);
+
+        let challengeRows = await driver.findElements(swd.By.css(".backbone.block-center"));
+        console.log(challengeRows);
+        await challengeRows[0].click();
+        
 
     } catch (err) {
         console.log(err);
     }
 })();
 
- async function loginHelper() {
+async function loginHelper() {
+    await driver.manage().setTimeouts({ implicit: 10000, pageLoad: 10000 })
     let data = await fs.promises.readFile(cFile);
     let { user, pwd, url } = JSON.parse(data);
     // reaching at given url
@@ -35,4 +51,8 @@ let userToadd = process.argv[3];
     let LoginBtn = await driver.findElement(swd.By.css(".ui-btn.ui-btn-large.ui-btn-primary.auth-button"))
     //clicking log-in btn
     await LoginBtn.click();
+}
+async function waitForLoader() {
+    let loader = await driver.findElement(swd.By.css("#ajax-msg"));
+    await driver.wait(swd.until.elementIsNotVisible(loader));
 }
