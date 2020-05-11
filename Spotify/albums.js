1445275dc694f4b5546b459beaa88688d835fe23
Shivang <shivang.sharma15@outlook.com>
2020-05-11 19:32:34 +0530

    added new file

---
+++ Spotify/albums.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+        "name": "divide"
+    },
+    {
+        "name": "A star is born"
+    },
+    {
+        "name": "Hybrid Theory"
+    },
+    {
+        "name": "Minutes to Midnight"
+    },
+    {
+        "name": "Parachutes"
+    },
+    {
+        "name": "News of The World"
+    },
+    {
+        "name": "Night Visions"
+    }
+]
