3ed2782b91cc3d83c566b73e3ded77ac5d79f85f
Shivang <shivang.sharma15@outlook.com>
2020-07-03 18:15:15 +0530

    udpated index.js file

--- Electron/Activity/index.js
+++ Electron/Activity/index.js
@@ -2,6 +2,7 @@ const $ = require("jquery")
 
 $(document).ready(
     function(){
+        let db=[]
         $("#grid .cell").on("click",function(){
             let rowid = Number($(this).attr("row-id"));
             let colid = Number($(this).attr("col-id"));
@@ -13,8 +14,29 @@ $(document).ready(
             let id = $(this).attr("id");
             $(`#${id}-options`).addClass("selected");
         })
+
+        $("#new").on("click",function(){
+            db = [];
+           let rows = $("#grid").find(".row");
+           for(let i= 0 ;i <rows.length ; i++){
+               let row = [];
+               let cRowCells = $(rows[i]).find(".cell");
+               for(let j=0;j< cRowCells.length ; j++){
+                   //db
+                   let cell = false;
+                   row.push(cell);
+                   //ui
+                   $(cRowCells[j]).html("false");
+               }
+               db.push(row);
+           }
+           console.log(db);
+        })
+
+
         function init(){
             $("#file").trigger("click");
+            $("#new").click();
         }
         init();
     }
