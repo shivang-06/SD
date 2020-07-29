ed83fd8d324bbd597ac934de13d7f8a26ac54561
Shivang <shivang.sharma15@outlook.com>
2020-07-29 19:43:39 +0530

    updated index.js and css

--- Electron/VS-Clone/index.js
+++ Electron/VS-Clone/index.js
@@ -21,13 +21,13 @@ $(document).ready(function () {
         },
     }).on("open_node.jstree", function (e, data) {
         let children = data.node.children;
-        
+
         for (let i = 0; i < children.length; i++) {
             let gcNodes = createChildNode(children[i]);
-            
+
             for (let j = 0; j < gcNodes.length; j++) {
                 // data array 
-                
+
                 console.log(children[i])
                 let isGcPresent = $('#tree').jstree(true).get_node(gcNodes[j].id);
                 if (isGcPresent) {
@@ -36,11 +36,11 @@ $(document).ready(function () {
                 $("#tree").jstree().create_node(children[i], gcNodes[j], "first");
             }
         }
-    }).on("select_node.jstree",function(e,data){ //event for select on file explorer.
+    }).on("select_node.jstree", function (e, data) { //event for select on file explorer.
         console.log("select event occured");
         let src = data.node.id;
         let isFile = fs.lstatSync(src).isFile(); //to check if the event clicked in file explorer is a file or a directory
-        if(!isFile){ //if it is not a file then don't open in code editor
+        if (!isFile) { //if it is not a file then don't open in code editor
             return;
         }
         let content = fs.readFileSync(src) + ""; //if a file, can be opened in 
@@ -64,9 +64,28 @@ function createChildNode(src) {
         }
         chArr.push(chObj);
     }
-    return chArr;   
+    return chArr;
 }
 
-function createEditor(){
+function createEditor() {
+    const path = require('path');
+    const amdLoader = require('./node_modules/monaco-editor/min/vs/loader.js');
+    const amdRequire = amdLoader.require;
+    const amdDefine = amdLoader.require.define;
+    amdRequire.config({
+        baseUrl: './node_modules/monaco-editor/min'
+    });
+    // workaround monaco-css not understanding the environment
+    self.module = undefined;
+    amdRequire(['vs/editor/editor.main'], function () {
+        var editor = monaco.editor.create(document.getElementById('code-editor'), {
+            value: [
+                'function x() {',
+                '\tconsole.log("Hello world!");',
+                '}'
+            ].join('\n'),
+            language: 'javascript'
+        });
+    });
 
 }
