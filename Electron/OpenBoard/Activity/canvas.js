e2e8fcf015fcfc27fbb91f507a3a30d4732a1e28
Shivang <shivang.sharma15@outlook.com>
2020-08-10 18:43:53 +0530

    updated upto icons

--- Electron/OpenBoard/Activity/canvas.js
+++ Electron/OpenBoard/Activity/canvas.js
@@ -1,31 +1,38 @@
 // press mouse
 let isPenDown = false;
-console.log("We are in canvas");
 board.addEventListener("mousedown", function (e) {
     // begin path
     ctx.beginPath();
     // move to mouse pointers location
     let x = e.clientX;
     let y = e.clientY;
+    let top = getLocation();
+    y = Number(y) - top
     ctx.moveTo(x, y);
-    console.log("MOuse down")
+    console.log("Mouse down")
     isPenDown = true;
 })
 // on move
 board.addEventListener("mousemove", function (e) {
     if (isPenDown) {
-        console.log("MOuse move")
+        console.log("Mouse move")
         // lineTo
         let x = e.clientX;
         let y = e.clientY;
+        let top = getLocation();
+        y = Number(y) - top;
         ctx.lineTo(x, y);
         // stroke
         ctx.stroke();
     }
 })
-board.addEventListener("mouseup", function () {
+window.addEventListener("mouseup", function () {
     // close Path
-    console.log("MOuse up")
+    console.log("Mouse up")
     // ctx.closePath();
     isPenDown = false;
 })
+function getLocation() {
+    let { top } = board.getBoundingClientRect();
+    return top;
+}
