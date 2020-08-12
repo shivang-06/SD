45204822b08b5a63db0864065627d20f664c7ca8
Shivang <shivang.sharma15@outlook.com>
2020-08-12 15:13:31 +0530

    updated upto sticky move

--- Electron/OpenBoard/Activity/tools.js
+++ Electron/OpenBoard/Activity/tools.js
@@ -1,8 +1,12 @@
 ctx.lineWidth = 5;
-
+//To make drawing circular and smooth
 ctx.lineCap="round";
 ctx.lineJoin='round';
+
+//setting pencil by default when app is opened
 let activeTool = 'pencil';
+
+
 let pencil = document.querySelector("#pencil");
 let eraser = document.querySelector("#eraser");
 let pencilOptions = document.querySelector("#pencil-options");
@@ -28,10 +32,11 @@ function handleTool(tool) {
         createSticky();
     }
 }
+//Handling color change
 function changeColor(color) {
     ctx.strokeStyle = color;
 }
-
+//Handling size change of tool
 let sliders = document.querySelectorAll("input[type='range']");
 for (let i = 0; i < sliders.length; i++) {
     sliders[i].addEventListener("change", function () {
