1481324b25d5054cdf422d441a6ece7380ee18a6
Shivang <shivang.sharma15@outlook.com>
2020-08-18 00:59:15 +0530

    updated upto sockets

--- Electron/OpenBoard/server/app.js
+++ Electron/OpenBoard/server/app.js
@@ -1,10 +1,14 @@
 //npm install express
 const express = require("express");
 const app = express();
+//server
+const httpServer = require("http").createServer(app);
+const socketServer = require("socket.io")(httpServer);
 
-app.use(express.static("activity"));
-app.get("/home",function(req,res){
-    res.sendFile(path.join(__dirname,"Activity/index.html"));
+app.use(express.static("Activity"));
+socketServer.on("connection",function(socket){
+    console.log("New client connected");
+    console.log(socket.id);
 })
 //tcp => uniquely identify server on a machine
 
