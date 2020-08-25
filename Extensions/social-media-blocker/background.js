be1d8854f347908f0909998dac85153245671f2a
Shivang <shivang.sharma15@outlook.com>
2020-08-25 17:44:10 +0530

    updated upto create site list and sendTobackground

--- Extensions/social-media-blocker/background.js
+++ Extensions/social-media-blocker/background.js
@@ -1,1 +1,10 @@
 console.log("this is background");
+let blockedSites = [];
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        console.log(request);
+        blockedSites.push(request);
+        console.log(blockedSites)
+        sendResponse("Hello from content");
+    })
--- Extensions/social-media-blocker/script.js
+++ Extensions/social-media-blocker/script.js
@@ -1,15 +1,30 @@
 console.log("this is popup");
 const subBtn = document.querySelector("button");
-subBtn.addEventListener("click",function(){
+const ul = document.querySelector("ul");
+subBtn.addEventListener("click",async function(){
     const val = document.getElementById("input").value;
-    console.log(val);
+
+    await sendToBackground(val);
     displayOnPopup(val);
 })
 
 function displayOnPopup(val){
-    let ul = document.createElement("ul");
     let li = document.createElement("li");
-    li.innerHTML = val;
+    li.innerHTML = `${val} <i class="fas fa-times"></i>`;
+    li.classList.add("list-group-item");
     ul.appendChild(li);
-    document.body.appendChild(ul);
+    let i = li.querySelector("i");
+    i.addEventListener("click",function(){
+        // let isRemoved = removeFromDb(i.parentNode.textContent); //remove this site from database in background
+        // if(isRemoved){
+        // }
+        i.parentNode.remove();
+    })
+}
+function sendToBackground(msg){
+    return new Promise(function(resolve,reject){
+        chrome.runtime.sendMessage(msg,function(response){
+            resolve(true);
+        })
+    })
 }
