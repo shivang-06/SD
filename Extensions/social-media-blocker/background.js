24a7e41ea566d1ee9399d2b13cb3b1c710377736
Shivang <shivang.sharma15@outlook.com>
2020-08-25 18:39:36 +0530

    updated upto website tracker

--- Extensions/social-media-blocker/background.js
+++ Extensions/social-media-blocker/background.js
@@ -4,7 +4,39 @@ let blockedSites = [];
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         console.log(request);
-        blockedSites.push(request);
-        console.log(blockedSites)
+        blockedSites.push({ site: request, time: 10 });
         sendResponse("Hello from content");
     })
+
+async function init() {
+    if (blockedSites.length > 0) {
+        // query for current tab
+        let tab = await getCurrentTab();
+
+        if (tab) {
+            let cTabUrl = tab.url;
+            for (let i = 0; i < blockedSites.length; i++) {
+                let isMatching = cTabUrl.includes(blockedSites[i].site);
+                if (isMatching) {
+                    blockedSites[i].time--;
+                    console.log("time remaining  "+ blockedSites[i].time);
+                    if (blockedSites[i].time <= 0) {
+                        // close current tab
+                        console.log("closed"+ blockedSites[i].site);
+                    }
+                }
+
+            }
+        }
+
+    }
+}
+function getCurrentTab() {
+    return new Promise(function (resolve, reject) {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            resolve(tabs[0]);
+        })
+    })
+
+}
+setInterval(init, 1000);
