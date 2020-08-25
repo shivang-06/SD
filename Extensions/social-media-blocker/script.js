d8da1d2b857353be915c17d19947108955cce653
Shivang <shivang.sharma15@outlook.com>
2020-08-25 16:57:34 +0530

    updated upto add wesite list

--- Extensions/social-media-blocker/script.js
+++ Extensions/social-media-blocker/script.js
@@ -1,1 +1,15 @@
 console.log("this is popup");
+const subBtn = document.querySelector("button");
+subBtn.addEventListener("click",function(){
+    const val = document.getElementById("input").value;
+    console.log(val);
+    displayOnPopup(val);
+})
+
+function displayOnPopup(val){
+    let ul = document.createElement("ul");
+    let li = document.createElement("li");
+    li.innerHTML = val;
+    ul.appendChild(li);
+    document.body.appendChild(ul);
+}
